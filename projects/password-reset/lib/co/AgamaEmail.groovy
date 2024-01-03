71065620d16d1df0081ed73132fa0b9ecc8d6bf7
Mostafejur Rahman <mrraju.ice.iu@gmail.com>
2024-01-04 01:37:22 +0600

    feat: update AgamaEmail.groovy

--- projects/password-reset/lib/co/AgamaEmail.groovy
+++ projects/password-reset/lib/co/AgamaEmail.groovy
@@ -5,13 +5,28 @@ import javax.mail.internet.*
 
 class AgamaEmail {
 
-    static boolean send(Obj message) {
-        smtp_server = "mail.google.com"
-
+    static boolean send(String to) {
+    
         String email = "mrraju.ice.iu@gmail.com"
         String password = "orysnttzwbujqzus"
         String host = "smtp.google.com"
         String port ="587"
+        
+        Properties props = System.getProperties()
+        props.put("mail.smtp.user", email)
+        props.put("mail.smtp.host", host)
+        props.put("mail.smtp.port", port)
+        props.put("mail.smtp.starttls.enable","true")
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory")
+        props.put("mail.smtp.ssl.trust", host) 
+
+
+       MimeMessage message = new MimeMessage(Session.getDefaultInstance(props))
+       message.setFrom(new InternetAddress(addresser))
+       message.addRecipients(Message.RecipientType.TO, new InternetAddress(to))
+
+
+
     }
 
 }
