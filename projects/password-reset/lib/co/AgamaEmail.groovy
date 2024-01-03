decdd542718a0a87633d72d0a9565ba8bee4664a
Mostafejur Rahman <mrraju.ice.iu@gmail.com>
2024-01-04 01:22:51 +0600

    feat: update AgamaEmail.groovy

--- projects/password-reset/lib/co/AgamaEmail.groovy
+++ projects/password-reset/lib/co/AgamaEmail.groovy
@@ -1,8 +1,17 @@
 package co
+
+import javax.mail.*
+import javax.mail.internet.*
+
 class AgamaEmail {
 
     static boolean send(Obj message) {
         smtp_server = "mail.google.com"
+
+        String email = "mrraju.ice.iu@gmail.com"
+        String password = ""
+        String host = "smtp.google.com"
+        String port ="587"
     }
 
 }
