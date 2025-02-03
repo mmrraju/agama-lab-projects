a54eaf3aef72b286b76c675aa0022e3b5b9812f2
Mostafejur Rahman <mrraju.ice.iu@gmail.com>
2025-02-04 02:49:19 +0600

    feat: add UserRegistration.java

---
+++ projects/agama-registration/lib/org/gluu/agama/user/UserRegistration.java
@@ -0,0 +1,16 @@
+package org.gluu.agama.smtp.jans.user;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.gluu.agama.smtp.jans.user.registration.JansUserRegistration;
+
+
+public abstract class UserRegistration {
+
+    public abstract String addNewUser(Map<String, String> profile, Set<String> attributes) throws Exception;
+    
+    public static UserRegistration getInstance(){
+        return  JansUserRegistration.getInstance();
+    }
+}
