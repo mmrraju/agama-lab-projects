d212eb31fa5d8ef48c7e258ed72a28411e3f8bed
Mostafejur Rahman <mrraju.ice.iu@gmail.com>
2025-02-05 00:30:08 +0600

    refactor: update UserRegistration.java

--- projects/agama-registration/lib/org/gluu/agama/user/UserRegistration.java
+++ projects/agama-registration/lib/org/gluu/agama/user/UserRegistration.java
@@ -3,12 +3,12 @@ package org.gluu.agama.user;
 import java.util.Map;
 import java.util.Set;
 
-import org.gluu.agama.smtp.jans.user.registration.JansUserRegistration;
+import org.gluu.agama.user.registration.JansUserRegistration;
 
 
 public abstract class UserRegistration {
 
-    public abstract String addNewUser(Map<String, String> profile, Set<String> attributes) throws Exception;
+    public abstract String addNewUser(Map<String, String> profile) throws Exception;
     
     public static UserRegistration getInstance(){
         return  JansUserRegistration.getInstance();
