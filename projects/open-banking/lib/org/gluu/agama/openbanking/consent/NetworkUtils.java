4e21052fd022d3fd3f0cf52e9b4ebefa73a16f35
Mostafejur Rahman <mrraju.ice.iu@gmail.com>
2026-09-08 04:25:56 +0600

    feat: add NetworkUtils.java

---
+++ projects/open-banking/lib/org/gluu/agama/openbanking/consent/NetworkUtils.java
@@ -0,0 +1,11 @@
+package org.gluu.agama.openbanking.consent;
+
+import io.jans.service.cdi.util.CdiUtil;
+import jakarta.servlet.http.HttpServletRequest;
+
+public class NetworkUtils {
+    public static String urlBeforeContextPath() {
+        HttpServletRequest req = CdiUtil.bean(HttpServletRequest.class);
+        return req.getScheme() + "://" + req.getServerName();
+    }    
+}
